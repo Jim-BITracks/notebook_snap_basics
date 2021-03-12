4843bd3d8b74fee164fac9e863671fd42d7bdcaf
jsimpson3 <jsimpson@bitracks.com>
2021-03-12 17:32:37 -0500

    test file

--- content/executeprocess_test.sql
+++ content/executeprocess_test.sql
@@ -4,7 +4,7 @@
         "cell_name": "insert stg.d_vehicle",
         "working_directory": "python_script_path",
         "executable": "python",
-        "arguments_expr": "execute_snowsql_v2.py -eltsnap_server ${sys:EltsnapServer} -eltsnap_database ${sys:EltsnapDatabase} -eltsnap_project ${sys:ProjectName} -eltsnap_package ${sys:PackageName} -prg ${sys:PackageRunGIUD} -qkey execute_sql -fsrv <#${ELT_Framework_Server}#>'localhost'<##> -fdb <#${ELT_Framework_Database}#>'elt_framework'<##> -snowflake_Account <#${snowflake_Account}#>'jimm'<##> -snowflake_Region <#${snowflake_Region}#>'test'<##> -snowflake_User <#${snowflake_User}#>'jimm'<##> -snowflake_Password <#${snowflake_Password}#>'SnowBiz34%^'<##> -use_snowflake_Warehouse <#${snowflake_Warehouse}#>'DEV'<##> -use_snowflake_Database <#${snowflake_Database}#>'CAR_CRASH'<##> -use_snowflake_Schema <#${snowflake_Schema}#>'STG'<##>",
+        "arguments_expr": "execute_snowsql_v2.py -eltsnap_server ${sys:EltsnapServer} -eltsnap_database ${sys:EltsnapDatabase} -eltsnap_project ${sys:ProjectName} -eltsnap_package ${sys:PackageName} -prg ${sys:PackageRunGIUD} -qkey execute_sql -fsrv <#${ELT_Framework_Server}#>'localhost'<##> -fdb <#${ELT_Framework_Database}#>'elt_framework'<##> -snowflake_Account ${snowflake_Account} -snowflake_Region ${snowflake_Region} -snowflake_User <#${snowflake_User}#>'jimm'<##> -snowflake_Password <#${snowflake_Password}#>'SnowBiz34%^'<##> -use_snowflake_Warehouse <#${snowflake_Warehouse}#>'DEV'<##> -use_snowflake_Database <#${snowflake_Database}#>'CAR_CRASH'<##> -use_snowflake_Schema <#${snowflake_Schema}#>'STG'<##>",
         "pattern": "Execute Process"
     }
 }
