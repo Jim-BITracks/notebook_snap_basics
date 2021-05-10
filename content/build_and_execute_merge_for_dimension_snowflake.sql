c8bca465d86e565e37f2715bd39f91e37cd66b2a
jsimpson3 <jsimpson@bitracks.com>
2021-05-10 15:07:49 -0400

    removed CAST to varchar8000

--- content/build_and_execute_merge_for_dimension_snowflake.sql
+++ content/build_and_execute_merge_for_dimension_snowflake.sql
@@ -149,7 +149,7 @@ sql = `WITH base AS
 			   ON t1.COLUMN_NAME = c.COLUMN_NAME
 			ORDER BY c.ORDINAL_POSITION
 		)
-		SELECT TRIM(LISTAGG(' IFNULL(CAST(r.' || column_name || ' AS VARCHAR(8000)),'''') != IFNULL(CAST(s.' || column_name || ' AS VARCHAR(8000)),'''')' || ' OR '), 'OR ')
+		SELECT TRIM(LISTAGG(' IFNULL(r.' || column_name || ','''') != IFNULL(s.' || column_name || ','''')' || ' OR '), 'OR ')
 			FROM base;`
 cmd_res = snowflake.execute({sqlText: sql});
 cmd_res.next();
@@ -170,7 +170,7 @@ sql = `WITH base AS
 			   ON t1.COLUMN_NAME = c.COLUMN_NAME
 			ORDER BY c.ORDINAL_POSITION
 		)
-		SELECT TRIM(LISTAGG(' IFNULL(CAST(r.' || column_name || ' AS VARCHAR(8000)),'''') != IFNULL(CAST(s.' || column_name || ' AS VARCHAR(8000)),'''')' || ' OR '), 'OR ')
+		SELECT TRIM(LISTAGG(' IFNULL(r.' || column_name || ','''') != IFNULL(s.' || column_name || ','''')' || ' OR '), 'OR ')
 			FROM base;`
 cmd_res = snowflake.execute({sqlText: sql});
 cmd_res.next();
