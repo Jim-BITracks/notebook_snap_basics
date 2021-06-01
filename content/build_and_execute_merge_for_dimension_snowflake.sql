5840497e2320e75a04ef1bf97a1c94aab649a308
jsimpson3 <jsimpson@bitracks.com>
2021-06-01 16:48:59 -0400

    changed to -99999

--- content/build_and_execute_merge_for_dimension_snowflake.sql
+++ content/build_and_execute_merge_for_dimension_snowflake.sql
@@ -149,7 +149,7 @@ sql = `WITH base AS
 			   ON t1.COLUMN_NAME = c.COLUMN_NAME
 			ORDER BY c.ORDINAL_POSITION
 		)
-		SELECT TRIM(LISTAGG(' IFNULL(CAST(r.' || column_name || ' AS VARCHAR),'''') != IFNULL(CAST(s.' || column_name || ' AS VARCHAR),'''')' || ' OR '), 'OR ')
+		SELECT TRIM(LISTAGG(' IFNULL(CAST(r.' || column_name || ' AS VARCHAR),''-99999'') != IFNULL(CAST(s.' || column_name || ' AS VARCHAR),''-99999'')' || ' OR '), 'OR ')
 			FROM base;`
 cmd_res = snowflake.execute({sqlText: sql});
 cmd_res.next();
@@ -170,7 +170,7 @@ sql = `WITH base AS
 			   ON t1.COLUMN_NAME = c.COLUMN_NAME
 			ORDER BY c.ORDINAL_POSITION
 		)
-		SELECT TRIM(LISTAGG(' IFNULL(CAST(r.' || column_name || ' AS VARCHAR,'''') != IFNULL(CAST(s.' || column_name || ' AS VARCHAR,'''')' || ' OR '), 'OR ')
+		SELECT TRIM(LISTAGG(' IFNULL(CAST(r.' || column_name || ' AS VARCHAR,''-99999'') != IFNULL(CAST(s.' || column_name || ' AS VARCHAR,''-99999'')' || ' OR '), 'OR ')
 			FROM base;`
 cmd_res = snowflake.execute({sqlText: sql});
 cmd_res.next();
