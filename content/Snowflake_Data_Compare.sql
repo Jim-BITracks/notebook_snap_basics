353fc94d6d1a115587882d9140db4b4006015019
Jim Miller <jmiller@bitracks.com>
2021-06-29 10:45:28 -0400

    update

--- content/Snowflake_Data_Compare.sql
+++ content/Snowflake_Data_Compare.sql
@@ -7,42 +7,18 @@
     }
 }
 */
-CREATE OR REPLACE PROCEDURE STG.SNOWFLAKE_DATA_COMPARE (SRC_DATABASE VARCHAR, 
-														  SRC_SCHEMA VARCHAR, 
-														  SRC_TABLE VARCHAR,
-														  TGT_DATABASE VARCHAR,
-														  TGT_SCHEMA VARCHAR, 
-														  TGT_TABLE VARCHAR,  
-														  NATURAL_KEY_COLUMNS VARCHAR, 
-														  IGNORE_COLUMNS VARCHAR, 
-														  SOURCE_WHERE_CLAUSE VARCHAR, 
-														  DESTINATION_WHERE_CLAUSE VARCHAR,
-														  OUTPUT_DATABASE VARCHAR,
-														  OUTPUT_SCHEMA VARCHAR,
-														  OUTPUT_TABLE VARCHAR,
-														  CREATE_OUTPUT_TABLE VARCHAR)
-	RETURNS STRING 
-	LANGUAGE JAVASCRIPT
-	COMMENT = 'Created by: Jerry Simpson (BI Tracks Consulting)
-				Inputs:    
-					SRC_SCHEMA                  (required)
-					SRC_TABLE                   (required)
-					TGT_SCHEMA                  (required)
-					TGT_TABLE                   (required)
-					NATURAL_KEY_COLUMNS  		(required)
-					IGNORE_COLUMNS        		(required)
-					SOURCE_WHERE_CLAUSE         (optional)
-					DESTINATION_WHERE_CLAUSE    (optional)
-					SRC_DATABASE                (optional)
-					TGT_DATABASE                (optional)
-					OUTPUT_DATABASE VARCHAR 	(optional)
-					OUTPUT_SCHEMA VARCHAR 		(required)
-					OUTPUT_TABLE VARCHAR 		(required)
-					CREATE_OUTPUT_TABLE			(required)
-					'
-AS $$
+CREATE OR REPLACE PROCEDURE Z_DEBUG.SNOWFLAKE_DATA_COMPARE(SRC_DATABASE VARCHAR, SRC_SCHEMA VARCHAR, SRC_TABLE VARCHAR,
+                                        TGT_DATABASE VARCHAR, TGT_SCHEMA VARCHAR, TGT_TABLE VARCHAR,
+                                        NATURAL_KEY_COLUMNS VARCHAR, IGNORE_COLUMNS VARCHAR,
+                                        SOURCE_WHERE_CLAUSE VARCHAR, DESTINATION_WHERE_CLAUSE VARCHAR,
+                                        OUTPUT_DATABASE VARCHAR, OUTPUT_SCHEMA VARCHAR, OUTPUT_TABLE VARCHAR,
+                                        CREATE_OUTPUT_TABLE VARCHAR, DEBUG_MODE VARCHAR)
+    returns VARCHAR
+    language JAVASCRIPT
+    comment = 'Created by: Jerry Simpson (BI Tracks Consulting)'
+    as $$
 
-try 
+try
 {
 
 stg_table_full = SRC_DATABASE + '.' + SRC_SCHEMA + '.' + SRC_TABLE
@@ -57,19 +33,19 @@ if ( NATURAL_KEY_COLUMNS == '' )
 		return 'No Natural key(s) found'
 	}
 
-if ( SRC_DATABASE == '' )	
+if ( SRC_DATABASE == '' )
 	{
 		 stg_table_full = SRC_SCHEMA + '.' + SRC_TABLE
 		 stg_info_schema = SRC_DATABASE + 'INFORMATION_SCHEMA.COLUMNS'
 	}
- 
-if ( TGT_DATABASE == '' )	
+
+if ( TGT_DATABASE == '' )
 	{
 		 tgt_table_full = TGT_SCHEMA + '.' + TGT_TABLE
 		 tgt_info_schema = TGT_DATABASE + 'INFORMATION_SCHEMA.COLUMNS'
 	}
 
-if ( OUTPUT_DATABASE == '' )	
+if ( OUTPUT_DATABASE == '' )
 	{
 		 output_table_full = OUTPUT_SCHEMA + '.' + OUTPUT_TABLE
 	}
@@ -81,37 +57,37 @@ if ( IGNORE_COLUMNS != '' )
 		cmd_res.next();
 		ignore_columns_list = cmd_res.getColumnValue(1);
 	}
-else 
+else
 	{
 		ignore_columns_list = `''`
 	}
 
 
-if ( SOURCE_WHERE_CLAUSE != '' ) 
+if ( SOURCE_WHERE_CLAUSE != '' )
 	{
 	SOURCE_WHERE = SOURCE_WHERE_CLAUSE.split("'").join("''")
 	}
-else 
+else
 	{
 	SOURCE_WHERE = ''
 	}
 
 
-if ( DESTINATION_WHERE_CLAUSE != '' ) 
+if ( DESTINATION_WHERE_CLAUSE != '' )
 	{
 	TARGET_WHERE = DESTINATION_WHERE_CLAUSE.split("'").join("''")
 	}
-else 
+else
 	{
 	TARGET_WHERE = ''
 	}
 
 
-//Output table check 
-sql = `SELECT IFF( EXISTS (SELECT 1 FROM ` + stg_info_schema + ` 
-		WHERE TABLE_SCHEMA = '` + OUTPUT_SCHEMA + `' 
+//Output table check
+sql = `SELECT IFF( EXISTS (SELECT 1 FROM ` + stg_info_schema + `
+		WHERE TABLE_SCHEMA = '` + OUTPUT_SCHEMA + `'
 		AND TABLE_NAME = '` + OUTPUT_TABLE + `'), 'EXISTS', '');`
-cmd_res = snowflake.execute({sqlText: sql}); 
+cmd_res = snowflake.execute({sqlText: sql});
 cmd_res.next();
 
 if ( cmd_res.getColumnValue(1) != 'EXISTS' && CREATE_OUTPUT_TABLE != 'Y' && CREATE_OUTPUT_TABLE != 'y' )
@@ -121,9 +97,9 @@ if ( cmd_res.getColumnValue(1) != 'EXISTS' && CREATE_OUTPUT_TABLE != 'Y' && CREA
 
 
 //Populate temp table with staging table columns
-sql = `CREATE OR REPLACE TEMPORARY TABLE src_table AS 
+sql = `CREATE OR REPLACE TEMPORARY TABLE src_table AS
 			SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, 'NULL' AS IS_NULLABLE, DATA_TYPE
-			, CASE WHEN DATA_TYPE IN ('NUMBER','DECIMAL') THEN NUMERIC_PRECISION::VARCHAR || ',' || NUMERIC_SCALE::VARCHAR ELSE CHARACTER_MAXIMUM_LENGTH::VARCHAR END AS "COLUMN_LENGTH" 
+			, CASE WHEN DATA_TYPE IN ('NUMBER','DECIMAL') THEN NUMERIC_PRECISION::VARCHAR || ',' || NUMERIC_SCALE::VARCHAR ELSE CHARACTER_MAXIMUM_LENGTH::VARCHAR END AS "COLUMN_LENGTH"
 			, COLLATION_NAME
 			FROM ` + stg_info_schema + `
 			WHERE TABLE_SCHEMA = '` + SRC_SCHEMA + `'
@@ -134,9 +110,9 @@ cmd_res = snowflake.execute({sqlText: sql});
 
 
 //Populate temp table with destination table columns
-sql = `CREATE OR REPLACE TEMPORARY TABLE tgt_table AS 
+sql = `CREATE OR REPLACE TEMPORARY TABLE tgt_table AS
 			SELECT TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME, ORDINAL_POSITION, 'NULL' AS IS_NULLABLE, DATA_TYPE
-			, CASE WHEN DATA_TYPE IN ('NUMBER','DECIMAL') THEN NUMERIC_PRECISION::VARCHAR || ',' || NUMERIC_SCALE::VARCHAR ELSE CHARACTER_MAXIMUM_LENGTH::VARCHAR END AS "COLUMN_LENGTH" 
+			, CASE WHEN DATA_TYPE IN ('NUMBER','DECIMAL') THEN NUMERIC_PRECISION::VARCHAR || ',' || NUMERIC_SCALE::VARCHAR ELSE CHARACTER_MAXIMUM_LENGTH::VARCHAR END AS "COLUMN_LENGTH"
 			, COLLATION_NAME
 			FROM ` + tgt_info_schema + `
 			WHERE TABLE_SCHEMA = '` + TGT_SCHEMA + `'
@@ -151,7 +127,7 @@ sql = `CREATE OR REPLACE TEMPORARY TABLE NATURAL_KEY_COLUMNS AS
 		WITH CTE (KEY_COLUMN) AS (
 		SELECT '` + NATURAL_KEY_COLUMNS + `' AS KEY_COLUMN)
 		SELECT TRIM(VALUE) AS COLUMN_NAME
-		FROM CTE, table(SPLIT_TO_TABLE(KEY_COLUMN,','));` 
+		FROM CTE, table(SPLIT_TO_TABLE(KEY_COLUMN,','));`
 cmd_res = snowflake.execute({sqlText: sql});
 
 
@@ -159,7 +135,7 @@ cmd_res = snowflake.execute({sqlText: sql});
 sql = `WITH base AS
 		(
 		   SELECT DISTINCT TOP 1000 c.ORDINAL_POSITION
-				, c.COLUMN_NAME 
+				, c.COLUMN_NAME
 			 FROM src_table c
 			 JOIN NATURAL_KEY_COLUMNS m
 			   ON m.COLUMN_NAME = c.COLUMN_NAME
@@ -225,7 +201,7 @@ sql = `WITH base AS
 						, TO_TIMESTAMP_LTZ(CURRENT_DATE) AS COMPARE_INSERT_DATE
 						, ''` + tgt_table_full + `'' AS TABLE_NAME
 				        , ` + DstNatKeyValue + ` AS Natural_Key_Value
-				        , CASE WHEN tgt.' || COLUMN_NAME || ' <> src.' || COLUMN_NAME || ' THEN ''' || COLUMN_NAME || ''' END AS COLUMN_DIFFERENT              
+				        , CASE WHEN tgt.' || COLUMN_NAME || ' <> src.' || COLUMN_NAME || ' THEN ''' || COLUMN_NAME || ''' END AS COLUMN_DIFFERENT
 				        , src.' || COLUMN_NAME || ' AS Source_Value
 				        , tgt.' || COLUMN_NAME || ' AS Target_Value
 				FROM (SELECT DST_COLUMNS_HERE FROM ` + tgt_table_full + ' ' + TARGET_WHERE + `) tgt
@@ -234,11 +210,11 @@ sql = `WITH base AS
 				WHERE COLUMN_DIFFERENT IS NOT NULL' || '\n UNION ~') || '~', 'UNION ~~'),'~') AS "DIFFERENCE_SELECT"
 			FROM base;`
 cmd_res = snowflake.execute({sqlText: sql});
-cmd_res.next(); 
-StagingColumns = cmd_res.getColumnValue(1); 
+cmd_res.next();
+StagingColumns = cmd_res.getColumnValue(1);
 StagingWhereClause = cmd_res.getColumnValue(2);
-StagingColumnsNoAlias = cmd_res.getColumnValue(3); 
-Difference_SELECT = cmd_res.getColumnValue(4); 
+StagingColumnsNoAlias = cmd_res.getColumnValue(3);
+Difference_SELECT = cmd_res.getColumnValue(4);
 
 
 //Replace * with column list
@@ -257,12 +233,12 @@ sql = `WITH base AS
 			ORDER BY c.ORDINAL_POSITION
 		)
 		SELECT TRIM(LISTAGG(' tgt.' || COLUMN_NAME || '' || ', '), ', ')
-			   ,TRIM(LISTAGG( COLUMN_NAME || ', '), ', ') AS "TARGET_COLUMNS_NO_ALIAS" 
+			   ,TRIM(LISTAGG( COLUMN_NAME || ', '), ', ') AS "TARGET_COLUMNS_NO_ALIAS"
 			FROM base;`
 cmd_res = snowflake.execute({sqlText: sql});
-cmd_res.next(); 
-DstColumns = cmd_res.getColumnValue(1); 
-DstColumnsNoAlias = cmd_res.getColumnValue(2); 
+cmd_res.next();
+DstColumns = cmd_res.getColumnValue(1);
+DstColumnsNoAlias = cmd_res.getColumnValue(2);
 
 
 //Switch destination column list
@@ -278,8 +254,8 @@ sql = `WITH Difference_Check AS ( ` + Difference_SELECT_final + ` )
 								, NULL AS COLUMN_DIFFERENT
 								, NULL AS Source_Value
 								, NULL AS Target_Value
-		                  FROM ` + stg_table_full + ` src
-		                  LEFT JOIN ` + tgt_table_full + ` tgt
+		                  FROM (SELECT * FROM ` + stg_table_full + ` ` + SOURCE_WHERE.replace(/''/g, "'") + `) src
+		                  LEFT JOIN (SELECT * FROM ` + tgt_table_full + ` ` + TARGET_WHERE.replace(/''/g, "'") + `) tgt
 		                    ON ` + StagingNatKeyJoin + `
 		                  WHERE ` + DstNatKeyWHERE + `
 		                  )
@@ -290,21 +266,24 @@ sql = `WITH Difference_Check AS ( ` + Difference_SELECT_final + ` )
 								, NULL AS COLUMN_DIFFERENT
 								, NULL AS Source_Value
 								, NULL AS Target_Value
-		                  FROM ` + tgt_table_full + ` tgt
-		                  LEFT JOIN ` + stg_table_full + ` src
+		                  FROM (SELECT * FROM ` + tgt_table_full + ` ` + TARGET_WHERE.replace(/''/g, "'") + `) tgt
+		                  LEFT JOIN (SELECT * FROM ` + stg_table_full + ` ` + SOURCE_WHERE.replace(/''/g, "'") + `) src
 		                    ON ` + StagingNatKeyJoin + `
 		                  WHERE ` + StagingNatKeyWHERE + `
 		                  )
-		SELECT COMPARE_INSERT_DATE, TYPE_CODE, TABLE_NAME, Natural_Key_Value, COLUMN_DIFFERENT, Source_Value, Target_Value 
+		SELECT COMPARE_INSERT_DATE, TYPE_CODE, TABLE_NAME, Natural_Key_Value, COLUMN_DIFFERENT, Source_Value, Target_Value
 		FROM src_ONLY
-		UNION 
-		SELECT COMPARE_INSERT_DATE, TYPE_CODE, TABLE_NAME, Natural_Key_Value, COLUMN_DIFFERENT, Source_Value, Target_Value 
+		UNION
+		SELECT COMPARE_INSERT_DATE, TYPE_CODE, TABLE_NAME, Natural_Key_Value, COLUMN_DIFFERENT, Source_Value, Target_Value
 		FROM tgt_ONLY
 		UNION
 		SELECT COMPARE_INSERT_DATE, TYPE_CODE, TABLE_NAME, Natural_Key_Value, COLUMN_DIFFERENT, Source_Value, Target_Value
 		FROM Difference_Check
+        ORDER BY TYPE_CODE
 		`
-DifferenceStatement = sql
+DifferenceStatement = sql;
+
+if (DEBUG_MODE == 'Y') {  return sql };
 
 
 //Final statement
@@ -314,22 +293,17 @@ if ( CREATE_OUTPUT_TABLE == 'Y' || CREATE_OUTPUT_TABLE == 'y' )
 	cmd_res = snowflake.execute({sqlText: sql});
 	return 'Table ' + SRC_SCHEMA + '.DATA_COMPARE_TABLE successfully created.';
 	}
-else 
+else
 	{
 	sql = `INSERT INTO ` + output_table_full + ` \n` + DifferenceStatement
 	cmd_res = snowflake.execute({sqlText: sql});
 	cmd_res.next();
 	return cmd_res.getColumnValue(1) + ' Rows Inserted.';
 	}
-
-
 }
 catch (err)
 {
 return err.message
 }
 
-
 $$;
-
-
